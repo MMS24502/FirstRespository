06f043858742dcb587a4e4614aedd3034c934923
s24502 <s24502@pjwstk.edu.pl>
2024-03-22 19:24:25 +0100

    Zadanie 3

--- Zadanie1/Class/Program.cs
+++ Zadanie1/Class/Program.cs
@@ -7,3 +7,20 @@ var y = 5;
 x += y;
 
 Console.WriteLine(x);
+
+int[] tab = new[] { 5, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 12, 34, 12, 2, 3, 4 };
+var res = avg(tab);
+Console.WriteLine(res);
+
+
+static int  avg (int [] tab)
+{
+    var avg = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        avg += tab[i];
+    }
+
+    avg /= tab.Length;
+    return avg;
+}
